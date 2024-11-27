a0c9737f816643a8926d49bb15ebabae0935ab28
Dominic Furchert <dominic.furchert@edu.tbz.ch>
2024-11-27 23:09:40 +0100

    Assignment 10 - Task 3 Game of Life :)

--- 10-various-challenges/3-whitebox-testing-game-of-life/task/script.py
+++ 10-various-challenges/3-whitebox-testing-game-of-life/task/script.py
@@ -1,15 +1,111 @@
 #!/usr/bin/env python3
 
 def evolve(world, steps):
-    # implement this function
-    return "", -1 # placeholder
+    check_field_formal(world)
+    check_field_inside(world)
+    if type(steps) != int or steps < 1:
+        raise Warning("Choose at least one step")
+
+    new_field = populate_new_field(world, steps)
+
+    return new_field, count_cells(new_field) # placeholder
+
+def populate_new_field(old_field, pnf_steps):
+
+    for q in range(pnf_steps):
+        tmp_field = [old_field[0]]
+
+        for x in range(1,len(old_field)-1):
+            tmp_str = "|"
+            for y in range(1,len(old_field[x])-1):
+                n = get_neighbours(x, y, old_field)
+                if old_field[x][y] == "#" and n <= 1:
+                    tmp_str += " "
+                elif old_field[x][y] == "#" and n >= 4:
+                    tmp_str += " "
+                elif old_field[x][y] == "#":
+                    tmp_str += "#"
+                elif old_field[x][y] == " " and n == 3:
+                    tmp_str += "#"
+                else:
+                    tmp_str += " "
+
+            tmp_str += "|"
+            #print(tmp_str)
+            tmp_field.append(tmp_str)
+            #print(tmp_field)
+        tmp_field.append(old_field[-1])
+
+        old_field = tmp_field
+
+        for row in old_field:
+            print(row)
+
+        print("xxxxxxxxxxxxxxxxxxx")
+
+    return tuple(old_field)
+
+def check_field_formal(cff_field):
+    if type(cff_field) != tuple:
+        raise Warning("Field isn't a tuple")
+
+    if len(cff_field) < 3 or len(cff_field[0]) < 3:
+        raise Warning("Field to little")
+
+    field_len = len(cff_field[0])
+    for line in cff_field:
+        if type(line) != str:
+            raise Warning("Field doesn't consist of strings")
+        if len(line) != field_len:
+            raise Warning("Field width inconsistent")
+
+def check_field_inside(cfi_field):
+    for char in cfi_field[0]:
+        if char != "-":
+            raise Warning("Upper border is wrong")
+    for char in cfi_field[-1]:
+        if char != "-":
+            raise Warning("Lower border is wrong")
+
+    for n in range(1,len(cfi_field)-1):
+        if cfi_field[n][0] != "|":
+            raise Warning("Left border is wrong")
+        if cfi_field[n][-1] != "|":
+            raise Warning("Right border is wrong")
+
+        for q in range(1,len(cfi_field[n])-1):
+            if cfi_field[n][q] not in " #":
+                raise Warning(f"Bad field content: {cfi_field[n][q]}")
+
+def get_neighbours(x,y,gn_field):
+    gn_sum = 0
+
+    for xx in range(-1,2):
+        for yy in range (-1,2):
+            if gn_field[x+xx][y+yy] == "#":
+                gn_sum += 1
+
+    if gn_field[x][y] == "#":
+        gn_sum -= 1
+
+    return gn_sum
+
+def count_cells(cc_field):
+    cc_sum = 0
+
+    for line in cc_field:
+        for char in line:
+            if char == "#":
+                cc_sum += 1
+
+    return cc_sum
 
 field = (
     "--------------",
     "|            |",
-    "|   ###      |",
+    "|            |",
+    "|   ##       |",
     "|   #        |",
-    "|    #       |",
     "|            |",
     "--------------"
 )
@@ -21,4 +117,3 @@ print(f"Game of Life after {steps} moves:")
 for row in result:
     print(row)
 print(f"{alive_cells} are alive.")
-
--- 10-various-challenges/3-whitebox-testing-game-of-life/task/tests.py
+++ 10-various-challenges/3-whitebox-testing-game-of-life/task/tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from unittest import TestCase
-from task.script import evolve
+from script import evolve
 
 
 # You are supposed to develop the functionality in a test-driven way.
@@ -14,6 +14,10 @@ from task.script import evolve
 # _directly_ includes an assertion in the body, or -otherwise- the
 # grading will mark the test suite as invalid.
 class EvolveTestSuite(TestCase):
+
+    def _assert_return_type(self, actual, expected):
+        m = f"The evolve function should return a {type(expected)} but returned a {type(actual)}!"
+        self.assertIsInstance(actual, type(expected), m)
 
     def test_glider(self):
         field = (
@@ -35,5 +39,242 @@ class EvolveTestSuite(TestCase):
             "--------------"
         ), 5)
         actual = evolve(field, 4)
-        # self.assert...
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
 
+    def test_input_tuple_1(self):
+        with self.assertRaises(Warning):
+            evolve(1, 4)
+
+    def test_input_tuple_2(self):
+        field = ("--------------",2)
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_char(self):
+        s = '!"$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~üöäÜÖÄèéàÈÉÀ¨£'
+        for x in s:
+            field = (
+                "--------------",
+                f"|         {x}  |",
+                "|  ###       |",
+                "|  #         |",
+                "|   #        |",
+                "|            |",
+                "--------------"
+            )
+            with self.assertRaises(Warning):
+                evolve(field, 4)
+
+    def test_input_length_1(self):
+        field = (
+            "-------------",
+            "|            |",
+            "|  ###       |",
+            "|  #         |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_length_2(self):
+        field = (
+            "--------------",
+            "|           |",
+            "|  ###       |",
+            "|  #         |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_length_3(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|  ###       |",
+            "|  #         |",
+            "|   #        |",
+            "|            |",
+            "-------------"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_size_1(self):
+        field = (
+            "--------------",
+            "--------------"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_size_2(self):
+        field = (
+            "--",
+            "||",
+            "--"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 4)
+
+    def test_input_steps_1(self):
+        field = (
+            "--",
+            "| |",
+            "--"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, 0)
+
+    def test_input_steps_2(self):
+        field = (
+            "--",
+            "| |",
+            "--"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, -1)
+
+    def test_input_steps_3(self):
+        field = (
+            "--",
+            "| |",
+            "--"
+        )
+        with self.assertRaises(Warning):
+            evolve(field, "x")
+
+    def test_game_mini(self):
+        field = (
+            "---",
+            "|#|",
+            "---"
+        )
+        expected = ((
+            "---",
+            "| |",
+            "---"
+        ), 0)
+        actual = evolve(field, 4)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
+
+    def test_game_2(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|            |",
+            "|    #       |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        expected = ((
+            "--------------",
+            "|            |",
+            "|            |",
+            "|            |",
+            "|            |",
+            "|            |",
+            "--------------"
+        ), 0)
+        actual = evolve(field, 4)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
+
+    def test_game_3(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|            |",
+            "|   ##       |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        expected = ((
+            "--------------",
+            "|            |",
+            "|            |",
+            "|   ##       |",
+            "|   ##       |",
+            "|            |",
+            "--------------"
+        ), 4)
+        actual = evolve(field, 1)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
+
+    def test_game_inf(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|            |",
+            "|   ##       |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        expected = ((
+            "--------------",
+            "|            |",
+            "|            |",
+            "|   ##       |",
+            "|   ##       |",
+            "|            |",
+            "--------------"
+        ), 4)
+        actual = evolve(field, 30)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
+
+    def test_game_4(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|  ###       |",
+            "|  #         |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        expected = ((
+            "--------------",
+            "|   #        |",
+            "|  ##        |",
+            "|  # #       |",
+            "|            |",
+            "|            |",
+            "--------------"
+        ), 5)
+        actual = evolve(field, 1)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)
+
+    def test_game_5(self):
+        field = (
+            "--------------",
+            "|            |",
+            "|  ###       |",
+            "|  #         |",
+            "|   #        |",
+            "|            |",
+            "--------------"
+        )
+        expected = ((
+            "--------------",
+            "| ###        |",
+            "| #          |",
+            "|  #         |",
+            "|            |",
+            "|            |",
+            "--------------"
+        ), 5)
+        actual = evolve(field, 4)
+        self._assert_return_type(actual, expected)
+        self.assertEqual(expected,actual)

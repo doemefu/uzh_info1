3558270073a8538a66c8ae0a25f196b3b93bcf0f
Dominic Furchert <dominic.furchert@edu.tbz.ch>
2024-11-27 21:09:42 +0100

    Assignment 10 - Task 2 :)

--- 10-various-challenges/2-study-results/task/script.py
+++ 10-various-challenges/2-study-results/task/script.py
@@ -6,13 +6,26 @@ import os
 # Do not rename the function or change its list of parameters!
 def get_average_grade(path):
     if not os.path.exists(path):
-        pass # what to do?
-    return -1
+        return None
 
+    with open(path, "r") as fi:
+        content = fi.read()
+
+        grades = []
+
+        for line in content.splitlines():
+            if len(line) == 0 or line[0] == "#":
+                continue
+            else:
+                grade = line.split(":")
+
+                grades.append(float(grade[1]))
+
+    return round(sum(grades)/len(grades),2) if len(grades) != 0 else 0.0
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(get_average_grade("task/my_grades.txt"))
+print(get_average_grade("./my_grades.txt"))
 
